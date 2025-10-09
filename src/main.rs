51cc2afef129a2e08ab7bec2fbd4eba79b79abcd
Twelfthonehone <honehone.41games@gmail.com>
2025-10-09 14:10:32 +0900

    generating anime name only

--- src/main.rs
+++ src/main.rs
@@ -50,13 +50,12 @@ async fn main() -> anyhow::Result<()> {
         }
 
         // if !gen_map.contains_key(&doc.id.to_hex()) {
+        //     let mut q = form_urlencoded::Serializer::new(String::new());
         //     let item_type = match doc.item_type {
         //         1 => "anime",
         //         2 => "character",
         //         _ => bail!("invalid item type {}", doc.item_type),
         //     };
-
-        //     let mut q = form_urlencoded::Serializer::new(String::new());
         //     q.append_pair("item-type", item_type);
         //     let url = format!("{base_url_vec}/{}?{}", doc.id, q.finish());
         //     let url_jp = format!("{url}&lang=ja");
